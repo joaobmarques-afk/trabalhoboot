91c283da476da0422546c66b32a4d9da4cbb8d5a
Joao Pedro de Barros Marques <joao.bmarques@sempreceub.com>
2026-06-14 22:01:09 -0300

    fix: adicionando funcoes do banco e correcao de ssl

--- src/database.py
+++ src/database.py
@@ -1,16 +1,30 @@
 import os
+import certifi
 from pymongo import MongoClient
 from dotenv import load_dotenv
 
 load_dotenv()
 MONGO_URI = os.getenv("MONGO_URI")
 
-try:
-    client = MongoClient(MONGO_URI, serverSelectionTimeoutMS=5000)
-    client.admin.command('ping')
-    print("Conexão com o MongoDB Atlas estabelecida com sucesso!")
-    db = client["ecotracker_db"]
-    estoque_colecao = db["estoque"]
-except Exception as e:
-    print(f"X Erro critico ao conectar no MongoDB: {e}")
-    estoque_colecao = None
+client = MongoClient(MONGO_URI, serverSelectionTimeoutMS=5000, tlsCAFile=certifi.where())
+db = client["ecotracker_db"]
+estoque_colecao = db["estoque"]
+
+def inicializar_banco():
+    """Verifica a conexão com o banco de dados"""
+    try:
+        client.admin.command('ping')
+        print("Conexão com o MongoDB Atlas estabelecida com sucesso!")
+    except Exception as e:
+        print(f"X Erro critico ao conectar no MongoDB: {e}")
+
+def salvar_produto(produto):
+    """Insere um produto no banco de dados"""
+    try:
+        if estoque_colecao is not None:
+            estoque_colecao.insert_one(produto)
+            return True
+        return False
+    except Exception as e:
+        print(f"Erro ao salvar produto: {e}")
+        return False
